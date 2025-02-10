2cdaef2f5ed4474bf49fd7ba7bdc94f21275d593
Satoru Miura <satoru@miurasw.com>
2025-02-10 10:34:39 +0900

    changed all input fields to float

--- twoxstockfinderapp/models.py
+++ twoxstockfinderapp/models.py
@@ -6,35 +6,35 @@ class StockStatement(models.Model):
     id = models.AutoField(primary_key=True)
     code = models.IntegerField('銘柄コード', default=7105)  # 銘柄コード
     year = models.IntegerField('年度', default=2022)  # 年度
-    total_assets = models.IntegerField('総資産', blank=True, null=True)  # 総資産
-    net_assets = models.IntegerField('純資産', blank=True, null=True)  # 純資産
+    total_assets = models.FloatField('総資産', blank=True, null=True)  # 総資産
+    net_assets = models.FloatField('純資産', blank=True, null=True)  # 純資産
     shareholders_equity = models.FloatField('株主資本', blank=True, null=True)  # 株主資本
-    retained_earnings = models.IntegerField('利益剰余金', blank=True, null=True)  # 利益剰余金
-    short_term_loans = models.IntegerField('短期借入金', blank=True, null=True)  # 短期借入金
-    long_term_loans = models.IntegerField('長期借入金', default=131678000000)  # 長期借入金
-    bps = models.FloatField('BPS (1株あたり純資産)', default=592.01)  # BPS (1株あたり純資産)
+    retained_earnings = models.FloatField('利益剰余金', blank=True, null=True)  # 利益剰余金
+    short_term_loans = models.FloatField('短期借入金', blank=True, null=True)  # 短期借入金
+    long_term_loans = models.FloatField('長期借入金', blank=True, null=True, default=131678000000)  # 長期借入金
+    bps = models.FloatField('BPS (1株あたり純資産)', blank=True, null=True, default=592.01)  # BPS (1株あたり純資産)
     equity_ratio = models.FloatField('自己資本比率', blank=True, null=True)  # 自己資本比率
-    operating_cf = models.IntegerField('営業キャッシュフロー', blank=True, null=True)  # 営業キャッシュフロー
-    investing_cf = models.IntegerField('投資キャッシュフロー', default=-19243000000)  # 投資キャッシュフロー
-    financing_cf = models.IntegerField('財務キャッシュフロー', default=-4601000000)  # 財務キャッシュフロー
-    capital_expenditure = models.IntegerField('設備投資', blank=True, null=True)  # 設備投資
-    cash_equivalents = models.IntegerField('現金同等物', blank=True, null=True)  # 現金同等物
-    operating_cf_margin = models.FloatField('営業CFマージン', default=4.43)  # 営業CFマージン
-    revenue = models.IntegerField('売上高', default=465406000000)  # 売上高
-    operating_income = models.IntegerField('営業利益', blank=True, null=True)  # 営業利益
-    ordinary_income = models.IntegerField('経常利益', blank=True, null=True)  # 経常利益
-    net_income = models.IntegerField('純利益', blank=True, null=True)  # 純利益
+    operating_cf = models.FloatField('営業キャッシュフロー', blank=True, null=True)  # 営業キャッシュフロー
+    investing_cf = models.FloatField('投資キャッシュフロー', blank=True, null=True, default=-19243000000)  # 投資キャッシュフロー
+    financing_cf = models.FloatField('財務キャッシュフロー', blank=True, null=True, default=-4601000000)  # 財務キャッシュフロー
+    capital_expenditure = models.FloatField('設備投資', blank=True, null=True)  # 設備投資
+    cash_equivalents = models.FloatField('現金同等物', blank=True, null=True)  # 現金同等物
+    operating_cf_margin = models.FloatField('営業CFマージン', blank=True, null=True, default=4.43)  # 営業CFマージン
+    revenue = models.FloatField('売上高', blank=True, null=True, default=465406000000)  # 売上高
+    operating_income = models.FloatField('営業利益', blank=True, null=True)  # 営業利益
+    ordinary_income = models.FloatField('経常利益', blank=True, null=True)  # 経常利益
+    net_income = models.FloatField('純利益', blank=True, null=True)  # 純利益
     eps = models.FloatField('EPS (1株当たり利益)', blank=True, null=True)  # EPS (1株当たり利益)
-    roe = models.FloatField('ROE (自己資本利益率)', default=1.14)  # ROE (自己資本利益率)
+    roe = models.FloatField('ROE (自己資本利益率)', blank=True, null=True, default=1.14)  # ROE (自己資本利益率)
     roa = models.FloatField('ROA (総資産利益率)', blank=True, null=True)  # ROA (総資産利益率)
-    dividend_per_share = models.IntegerField('一株配当', blank=True, null=True)  # 一株配当
-    retained_earnings_dividends = models.IntegerField('剰余金の配当', blank=True, null=True)  # 剰余金の配当
-    share_buyback = models.IntegerField('自社株買い', blank=True, null=True)  # 自社株買い
-    dividend_payout_ratio = models.FloatField('配当性向', default=118.9)  # 配当性向
+    dividend_per_share = models.FloatField('一株配当', blank=True, null=True)  # 一株配当
+    retained_earnings_dividends = models.FloatField('剰余金の配当', blank=True, null=True)  # 剰余金の配当
+    share_buyback = models.FloatField('自社株買い', blank=True, null=True)  # 自社株買い
+    dividend_payout_ratio = models.FloatField('配当性向', blank=True, null=True, default=118.9)  # 配当性向
     total_return_ratio = models.FloatField('総還元性向', blank=True, null=True)  # 総還元性向
     net_asset_dividend_ratio = models.FloatField('純資産配当率', blank=True, null=True)  # 純資産配当率
-    result = models.IntegerField(blank=True, null=True)
-    company_name = models.CharField(max_length=100, default="三菱ロジスネクスト", blank=True, null=True)  # 会社名
+    result = models.FloatField(blank=True, null=True)
+    company_name = models.CharField(max_length=100, blank=True, null=True, default="三菱ロジスネクスト")  # 会社名
     proba = models.FloatField(blank=True, null=True)
     comment = models.CharField(max_length=200, blank=True, null=True)
     registered_date = models.DateField(default=date.today()) #default=date.today() : 本日の日付をデフォルトに設定
--- twoxstockfinderapp/views.py
+++ twoxstockfinderapp/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import InputForm, LoginForm
 from .models import StockStatement
 import requests
+from bs4 import BeautifulSoup
 
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.decorators import login_required
@@ -28,31 +29,31 @@ def result(request):
 
     # 推論の実行 (APIへリクエスト)
     x = {
-        "bps": statement.bps,
+        "bps": float(statement.bps) if statement.bps is not None else None,
         "long_term_loans": float(statement.long_term_loans) if statement.long_term_loans is not None else None,
-        "dividend_payout_ratio": statement.dividend_payout_ratio,
+        "dividend_payout_ratio": float(statement.dividend_payout_ratio) if statement.dividend_payout_ratio is not None else None,
         "revenue": float(statement.revenue) if statement.revenue is not None else None,
-        "roe": statement.roe,
+        "roe": float(statement.roe) if statement.roe is not None else None,
         "investing_cf": float(statement.investing_cf) if statement.investing_cf is not None else None,
-        "operating_cf_margin": statement.operating_cf_margin,
+        "operating_cf_margin": float(statement.operating_cf_margin) if statement.operating_cf_margin is not None else None,
         "financing_cf": float(statement.financing_cf) if statement.financing_cf is not None else None,
-        "shareholders_equity": statement.shareholders_equity,
+        "shareholders_equity": float(statement.shareholders_equity) if statement.shareholders_equity is not None else None,
         "retained_earnings": float(statement.retained_earnings) if statement.retained_earnings is not None else None,
         "cash_equivalents": float(statement.cash_equivalents) if statement.cash_equivalents is not None else None,
-        "roa": statement.roa,
+        "roa": float(statement.roa) if statement.roa is not None else None,
         "dividend_per_share": float(statement.dividend_per_share) if statement.dividend_per_share is not None else None,
         "retained_earnings_dividends": float(statement.retained_earnings_dividends) if statement.retained_earnings_dividends is not None else None,
         "net_assets": float(statement.net_assets) if statement.net_assets is not None else None,
         "total_assets": float(statement.total_assets) if statement.total_assets is not None else None,
         "net_income": float(statement.net_income) if statement.net_income is not None else None,
         "ordinary_income": float(statement.ordinary_income) if statement.ordinary_income is not None else None,
-        "net_asset_dividend_ratio": statement.net_asset_dividend_ratio,
-        "equity_ratio": statement.equity_ratio,
+        "net_asset_dividend_ratio": float(statement.net_asset_dividend_ratio) if statement.ordinary_income is not None else None,
+        "equity_ratio": float(statement.equity_ratio) if statement.equity_ratio is not None else None,
         "operating_income": float(statement.operating_income) if statement.operating_income is not None else None,
-        "total_return_ratio": statement.total_return_ratio,
+        "total_return_ratio": float(statement.total_return_ratio) if statement.total_return_ratio is not None else None,
         "short_term_loans": float(statement.short_term_loans) if statement.short_term_loans is not None else None,
         "capital_expenditure": float(statement.capital_expenditure) if statement.capital_expenditure is not None else None,
-        "eps": statement.eps,
+        "eps": float(statement.eps) if statement.eps is not None else None,
         "share_buyback": float(statement.share_buyback) if statement.share_buyback is not None else None,
         "operating_cf": float(statement.operating_cf) if statement.operating_cf is not None else None,
     }
@@ -61,7 +62,7 @@ def result(request):
     y_proba = response.json()["proba"]
 
     # 推論結果を保存
-    statement.company_name = ''
+    statement.company_name, current_price = get_company_name_and_current_price(statement.code)
     statement.proba = round(y_proba*100, 2)
     statement.result = y
     statement.save() # データを保存
@@ -73,8 +74,18 @@ def result(request):
         'year': statement.year,
         'result': statement.result,
         'proba': statement.proba,
+        'current_price': current_price,
     }
     return render(request, 'twoxstockfinderapp/result.html', result)
+
+def get_company_name_and_current_price(code):
+    response = requests.get(f'https://kabutan.jp/stock/kabuka?code={code}&ashi=yar')
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.text, 'html.parser')
+        div = soup.find('div', class_='si_i1_1')
+        company_name = div.find('h2').text.replace(f'{code}　', '').strip()
+        price = soup.find('span', class_='kabuka').text.replace('円', '').strip()
+    return company_name, float(price.replace(',', ''))
 
 @login_required
 def history(request):
